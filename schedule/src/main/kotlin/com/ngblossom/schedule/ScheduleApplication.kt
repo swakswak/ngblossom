78a1414ce75d22ff2f130b6a5b18234f75c805ce
swakswak <hschoi0702@gmail.com>
2023-06-04 01:46:20 +0900

    schedule 추가

---
+++ schedule/src/main/kotlin/com/ngblossom/schedule/ScheduleApplication.kt
@@ -0,0 +1,11 @@
+package com.ngblossom.schedule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScheduleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScheduleApplication>(*args)
+}
